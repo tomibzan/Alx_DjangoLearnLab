e26c814f244d99ae62780b037bf9acc283fd5b95
tomibzan <thomflowen@gmail.com>
2025-07-09 18:46:51 +0300

    django app make corrections

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField  # Create your models here.
+    publication_year = models.IntegerField(1900)
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.publication_year})"
