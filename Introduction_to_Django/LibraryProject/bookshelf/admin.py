27a2f8b094cfeee1502dee623f071d78ce8669a2
tomibzan <thomflowen@gmail.com>
2025-07-09 22:08:32 +0300

    added search fields to admin py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,6 +6,7 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     list_filter = ("title", "author", "publication_year")
+    search_fields = ["title", "author"]
 
 
 admin.site.register(Book)
