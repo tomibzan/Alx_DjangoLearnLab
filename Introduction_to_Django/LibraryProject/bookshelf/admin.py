c3ba6d3ecc221274a3690275dcb2705408de5e2a
tomibzan <thomflowen@gmail.com>
2025-07-09 21:54:47 +0300

    2nd admin correction

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    List_fiter = ("title", "author", "publication_year")
+
+
 admin.site.register(Book)
-admin.ModelAdmin.list_display = ('title', 'author', 'publication_year')
