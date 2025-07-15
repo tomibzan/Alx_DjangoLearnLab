3e28b44d7a97a3713ee5a9c40f1af643c14c3f9b
tomibzan <thomflowen@gmail.com>
2025-07-16 01:00:17 +0300

    code correction

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Book
 
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
