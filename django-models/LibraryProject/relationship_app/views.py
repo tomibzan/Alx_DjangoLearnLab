5483c7860bcc7eaa3c98d283ab49189a036bf05e
tomibzan <thomflowen@gmail.com>
2025-07-16 01:02:54 +0300

    code correction2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library
 from django.shortcuts import render
 from .models import Book
