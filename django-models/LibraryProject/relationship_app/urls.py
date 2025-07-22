bf6cbf7dbad406719d7e1c8da339b22cdaf0ecb7
tomibzan <thomflowen@gmail.com>
2025-07-22 21:18:54 +0300

    another debug

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'relationship_app'  # Namespace
-
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,27 +1,15 @@
 from django.views.generic.detail import DetailView
+from .models import Library
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Book, Library
-
-# Function-based view to list all books
+from .models import Book
 
 
 def list_books(request):
-    books = Book.objects.select_related(
-        'author').all()  # Efficient query with author
+    books = Book.objects.all()
     return render(request, 'list_books.html', {'books': books})
-
-
-# Class-based view to show library details
 
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
-
-    # Optional: override get_context_data if needed
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all()
-        return context
