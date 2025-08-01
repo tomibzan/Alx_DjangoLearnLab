b86e322d6993447af10cc72d28ad41e7ab58a463
tomibzan <thomflowen@gmail.com>
2025-08-01 23:18:56 +0300

    django models revisited 2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,8 @@
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
-from .views import list_books
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(),
          name='library_detail'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic.detail import DetailView
+from django.shortcuts import render
+from django.views.generic import DetailView
 from .models import Book, Library
+
+# Function-Based View: List all books
 
 
 def list_books(request):
-    books = Book.objects.all()
+    """View to list all books with their authors."""
+    books = Book.objects.select_related('author').all()  # Efficient query
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-Based View: Display Library Details
 
 
 class LibraryDetailView(DetailView):
@@ -16,14 +18,8 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('list_books')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add related books with their authors (efficient query)
+        context['books'] = self.object.books.all().select_related('author')
+        return context
