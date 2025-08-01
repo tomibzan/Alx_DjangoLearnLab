faf8a2e18e11e35e1e93e301fa4b439606c834fa
tomibzan <thomflowen@gmail.com>
2025-08-01 22:54:15 +0300

    django model task revisited1

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import Author, Book, Library, Librarian
 
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role')
-    list_filter = ('role',)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Librarian)
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = ('name', 'library')
--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,18 +4,32 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name='books')
     publication_year = models.IntegerField(default=2000)
+
+    def __str__(self):
+        return self.title
 
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
 
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    library = models.OneToOneField(
+        Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return f"{self.name} (Librarian of {self.library.name})"
