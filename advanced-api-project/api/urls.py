0c89bcbd266268c623c47a7ba7ee8ac473d3fb8e
tomibzan <thomflowen@gmail.com>
2025-08-10 17:55:44 +0300

    revisit task1 checker reauirements

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', views.BookCreateView.as_view(), name='book-create'),
-    path('books/update/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),
-    path('books/delete/<int:pk>/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/', views.ListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', views.DetailView.as_view(), name='book-detail'),
+    path('books/create/', views.CreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', views.UpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', views.DeleteView.as_view(), name='book-delete'),
 ]
