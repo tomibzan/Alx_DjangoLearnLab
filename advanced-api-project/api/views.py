cc72d14b5d820d7eed2b75a1662d9ad25df7ced1
tomibzan <thomflowen@gmail.com>
2025-08-10 18:04:14 +0300

    task 1 correction

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ Meets checker requirements for Task 1 and Task 2.
 """
 
 from rest_framework import generics, permissions, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework as django_filters
 from .models import Book
 from .serializers import BookSerializer
