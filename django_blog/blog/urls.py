1434ab622612bbaf826764c4c9bd561ba2c53940
tomibzan <thomflowen@gmail.com>
2025-08-15 00:00:53 +0300

    spellcheck

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -22,9 +22,9 @@ urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("profile/edit/", views.edit_profile, name="edit_profile"),
 
-    path('posts/<int:pk>/comments/new/', views.add_comment, name='add_comment'),
+    path('post/<int:pk>/comments/new/', views.add_comment, name='add_comment'),
     path('comment/<int:pk>/edit/', views.edit_comment, name='edit_comment'),
     path('comment/<int:pk>/update/', views.edit_comment, name='update_comment'),
     path('comment/<int:pk>/delete/', views.delete_comment, name='delete_comment'),
-    
+
 ]
