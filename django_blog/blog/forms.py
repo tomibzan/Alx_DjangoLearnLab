b3a1bfd0267ff03b2e466b8dd11c776d9f0a2d6a
tomibzan <thomflowen@gmail.com>
2025-08-15 12:59:20 +0300

    Added tagwidget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,16 +1,19 @@
 # blog/forms.py
 from django import forms
 from .models import Post, Comment
+from taggit.forms import TagWidget
 
 class PostForm(forms.ModelForm):
     tags = forms.CharField(required=False, help_text="Separate tags with commas")
-    
+
+
     class Meta:
         model = Post
         fields = ["title", "content"]
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter post title"}),
             "content": forms.Textarea(attrs={"class": "form-control", "rows": 10}),
+            "tags": TagWidget(),
         }
 
 class CommentForm(forms.ModelForm):
