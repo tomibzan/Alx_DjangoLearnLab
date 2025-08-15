ed59ff8cacd4714e90a9c6a4409257bfc9d6bd60
tomibzan <thomflowen@gmail.com>
2025-08-15 14:03:26 +0300

    updated

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from .views import PostByTagListView
+from .views import PostByTagListView, search_posts
 
 app_name = "blog"
 
