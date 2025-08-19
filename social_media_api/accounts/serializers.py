cd065b48b7c7b517cb706146f87e340c936a87eb
tomibzan <thomflowen@gmail.com>
2025-08-19 12:53:37 +0300

    corrrected serializer token

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,39 +1,41 @@
 # accounts/serializers.py
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    follower_count = serializers.ReadOnlyField()
-    following_count = serializers.ReadOnlyField()
-
-    class Meta:
-        model = CustomUser
-        fields = [
-            'id', 'username', 'email', 'first_name', 'last_name',
-            'bio', 'profile_picture', 'date_of_birth',
-            'follower_count', 'following_count', 'date_joined'
-        ]
-        read_only_fields = ['date_joined']
-
-
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        extra_kwargs = {
+            'email': {'required': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+        }
 
     def validate(self, attrs):
+        """Ensure passwords match"""
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Passwords don't match"})
+            raise serializers.ValidationError({"password": "Passwords don't match."})
         return attrs
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        """Create user and token"""
+        validated_data.pop('password2')
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data['first_name'],
@@ -41,3 +43,16 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+class UserSerializer(serializers.ModelSerializer):
+    follower_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name',
+            'bio', 'profile_picture', 'date_of_birth',
+            'follower_count', 'following_count', 'date_joined'
+        ]
+        read_only_fields = ['date_joined']
