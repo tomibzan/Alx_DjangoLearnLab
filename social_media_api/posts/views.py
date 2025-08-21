3c96f7cfc144b0b4a3cc006645b879484e9eaa1b
tomibzan <thomflowen@gmail.com>
2025-08-21 23:07:29 +0300

    corrected posts views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -66,7 +66,7 @@ class LikePostView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(post=post, user=request.user)
         if not created:
             return Response({"detail": "Already liked"}, status=status.HTTP_400_BAD_REQUEST)
